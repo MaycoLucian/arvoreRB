1b2bddcf55bc74f694b098350a3fd89706ad1876
mayco <mayco@Mayco-Lucian>
2018-10-14 16:43:48 -0300

    Implementação Arvore Rubro negra

--- src/br/com/estrutura/dados/arvore/Nodo.java
+++ src/br/com/estrutura/dados/arvore/Nodo.java
@@ -1,149 +1,121 @@
 package br.com.estrutura.dados.arvore;
 
 public class Nodo {
-
-	public Integer valor;
-	public Nodo p;
-	public Nodo esq;
-	public Nodo dir;
-	public boolean ver; // Uma flag booleana, que quando tiver valor "true",
-							// significa que o Nodo eh de cor vermelha, caso contrario eh preto
-
-	public Nodo(int valor, boolean ver) {
-		this.valor = valor;
-		this.ver = ver;
-		this.p = this.esq = this.dir = Arvore.nil;
-	}
-
-	public Nodo encontra(int n) {
-		if (n < this.valor && this.esq != Arvore.nil)
-			return this.esq.encontra(n);
-		else if (n > this.valor && this.dir != Arvore.nil)
-			return this.dir.encontra(n);
-		else
-			return this;
-	}
-
-	public Nodo maximo() {
-		if (this.dir != Arvore.nil)
-			return dir.maximo();
-		else
-			return this;
-	}
 	
 	/**
-	 * Vai mostrar o valor mais baixo presente na árvore a partir do Nodo que esta rodando no momento
-	 * @return
-	 */	
-	public Nodo minimo() {
-		if (this.esq != Arvore.nil) return esq.minimo();
-		else return this;
-	}
 
-	public void imprimirArvore() {
-		if (this.esq != Arvore.nil)
-			this.esq.imprimirArvore();
-		System.out.println(this.valor);
-		if (this.dir != Arvore.nil)
-			this.dir.imprimirArvore();
-	}
-
-	/**
-	 * Informa o valor do Nodo que antecede (em termos de valores) o atual Nodo
-	 */
-	public Nodo predecessor() {
-		if (this.esq != Arvore.nil)
-			return this.esq.maximo();
-		else
-			return this;
-	}
-
-	/**
-	 * Informa o valor do Nodo que sucede (em termos de valores) o atual Nodo
-	 */
-	public Nodo sucessor() {
-		if (this.dir != Arvore.nil)
-			return this.dir.minimo();
-		else
-			return this;
+	public Nodo avo(Nodo n) {
+	    if (n.getParent() == null) {
+	        return null; // Nao ter pai significa nao ter avo
+	    }
+	    Nodo pai = n.getParent();
+	    return pai.getParent(); // Se o pai tiver pai, retorna-o. Caso contrario, o 
+	    // retorno do metodo getParent sera null, e o retorno será null.
 	}
 	
-	public void grafico() {
-		if (this.ver) {
-				System.out.println("\t" + this.valor + " [style = filled, fillcolor = red];");
-		} else {
-				System.out.println("\t" + valor + " [style = filled, fillcolor = black, fontcolor = white];");
-		}
-
-		if (this.esq != Arvore.nil) {
-				System.out.println("\t" + valor + " -> " + this.esq.valor + " [label = \" esq\"];");
-				this.esq.grafico();
-		}
-		else {
-				System.out.println("\t" + valor + " -> nil [label = \" esq\"];");
-		}
-
-		if (this.dir != Arvore.nil) {
-				System.out.println("\t" + valor + " -> " + this.dir.valor + " [label = \" dir\"];");
-				this.dir.grafico();
-		}
-		else {
-				System.out.println("\t" + valor + " -> nil [label = \" dir\"];");
-		}
+	public Nodo tio(Nodo n) {
+	    Nodo avo = avo(n);
+	    if (avo == null) {
+	        return null;
+	    }
+	    Nodo pai = n.getParent();
+	    // Nao existe como saber se o pai eh maior ou menor que o avo, entao
+	    // para recuperar o tio, comparamos os dois.
+	    
+	    if (pai.compareTo(avo) > 0) { // Se pai > avo
+	        return avo.getLeft(); // retorna o filho da esquerda, que eh menor.
+	        // Caso nao exista, retorna null.
+	    } else { // Se pai < avo
+	        return avo.getRight(); // retorna o filho da direita, que eh maior.
+	        // Caso nao exista, retorna null.
+	    }
 	}
 	
-	public void encontra50(Integer q, int aux, Arvore res) {
-		if(q >= 50) return;
-
-		if (this.esq != Arvore.nil) {
-			this.esq.encontra50(q, aux, res);
-		}
-		if (this.valor > aux && q < 50) {
-			res.adiciona(this.valor);
-			q++;
-		}
-		if (this.dir != Arvore.nil) {
-			this.dir.encontra50(q, aux, res);
-		}
+	public void fixCase1(Nodo n) {
+	    if (n.getParent() == null) { // Se o no for raiz, pinta de preto.
+	        n.setColor(Color.BLACK);
+	    } else { // Se nao for, parte para o caso 2.
+	        fixCase2(n);
+	    }
 	}
-
-	public Integer getValor() {
-		return valor;
+	
+	public void fixCase2(Nodo n) {
+	    Color corDoPai = n.getParent().getColor();
+	    if (corDoPai.equals(Color.RED) { // Se o pai tiver cor vermelha, parte
+	        fixCase3(n); // para o caso 3. Se nao, a arvore esta correta.
+	    }
+	    
 	}
-
-	public void setValor(Integer valor) {
-		this.valor = valor;
+	
+	public void fixCase3(Nodo n) {
+	    // So se chega a este metodo se o pai do no for vermelho.
+	    // Verificaremos agora se o tio dele tambem eh vermelho.
+	    Nodo tio = tio(n); // Caso nao exista tio, a variavel recebe null.
+	    if (tio != null && tio.getColor().equals(Color.RED)) {
+	        // se existe tio e ele eh vermelho:
+	        // pinta o pai e o tio de preto, o avo de vermelho e roda o fixcase1 no
+	        // avo para fazer os ajustes, caso estes sejam necessarios.
+	        Nodo pai = n.getParent();
+	        pai.setColor(Color.BLACK);
+	        tio.setColor(Color.BLACK);
+	        
+	        Nodo avo = avo(n);
+	        avo.setColor(Color.RED);
+	        fixCase1(avo);
+	    } else {
+	        fixCase4(n); // Caso nao haja tio ou ele seja preto, parte ao caso 4.
+	    }
 	}
-
-	public Nodo getP() {
-		return p;
+	
+	public void fixCase4(Nodo n) {
+	    Nodo pai = n.getParent();
+	    Nodo avo = avo.getParent();
+	    
+	    if (n.compareTo(pai) > 0 && pai.compareTo(avo) < 0) {
+	        // Se o no eh filho da direita e o pai eh filho da esquerda
+	        rotaciona_esquerda(pai);
+	        
+	        n = n.getLeft(); // o no passa a ser o seu filho da esquerda, que
+	        // antes da rotacao era o seu pai.
+	    } else if (n.compareTo(pai) < 0 && pai.compareTo(avo) > 0) {
+	        // Se o no eh filho da esquerda e o pai eh filho da direita
+	        rotaciona_direita(pai);
+	        
+	        n = n.getRight(); // o no passa a ser o seu filho da direita, que
+	        // antes da rotacao era o seu pai.
+	    }
+	    fixCase5(n);
 	}
-
-	public void setP(Nodo p) {
-		this.p = p;
+	
+	public void fixCase5(Nodo n) {
+	    Nodo avo = avo(n);
+	    Nodo tio = tio(n);
+	    Nodo pai = n.getParent();
+	    
+	    // pinta o pai de preto e o avo de vermelho
+	    avo.setColor(Color.RED);
+	    pai.setColor(Color.BLACK);
+	    
+	    if (n.compareTo(pai) < 0 && pai.compareTo(avo) < 0) {
+	        // se o no eh filho da esquerda e o pai tambem, 
+	        // rotaciona o avo a direita.
+	        rotaciona_direita(avo);
+	    } else {
+	        // se nao, quer dizer que o no eh filho da direita e o pai tambem,
+	        // e rotacionamos o avo a esquerda.
+	        rotaciona_esquerda(avo);
+	    }
 	}
-
-	public Nodo getEsq() {
-		return esq;
-	}
-
-	public void setEsq(Nodo esq) {
-		this.esq = esq;
-	}
-
-	public Nodo getDir() {
-		return dir;
-	}
-
-	public void setDir(Nodo dir) {
-		this.dir = dir;
-	}
-
-	public boolean isVer() {
-		return ver;
-	}
-
-	public void setVer(boolean ver) {
-		this.ver = ver;
-	}
+	
+	public int blackHeightRecursive(Nodo no) {
+  		int altura = 0;
+  		if (!Nodo.isEmpty()) {
+  			if (no.getColour() == Colour.BLACK) {
+  				altura = 1 + Math.max(blackHeightRecursive(no.getLeft()), blackHeightRecursive(no.getRight()));
+  			} else {
+  				altura = Math.max(blackHeightRecursive(no.getLeft()), blackHeightRecursive(no.getRight()));
+  			}
+  		}
+  		return altura;
+  	} **/
 }
--- src/br/com/estrutura/dados/main/Main.java
+++ src/br/com/estrutura/dados/main/Main.java
@@ -1,60 +1,15 @@
 package br.com.estrutura.dados.main;
 
-import java.util.Random;
-
 import br.com.estrutura.dados.arvore.Arvore;
 
 public class Main {
-	 public static final int totalnodos = 100; // Nodos a serem inseridos randomicamente
-	  public static final int MAX = 2147483647; // 2^31 - 1 (para testes)
 
-	  public static void main(String args[]) {
-
-	    Random gerador = new Random(); // Para gerar números aleatórios
-	    long inicio, aux, media = 0, total;
-	    int chave;
-
-	    Arvore n50 = new Arvore();
-	    Arvore a = new Arvore();
-
-	    inicio = System.nanoTime(); // Contagem de tempo total
-
-	    // Inserir 200000 nodos
-	    for(int i = 0; i < totalnodos; i++) {
-	      aux = System.nanoTime(); // Contagem de tempo de inserção
-	      a.adiciona(gerador.nextInt(MAX));
-	      media += System.nanoTime() - aux;
-	    }
-
-	    media /= totalnodos;
-	    System.out.printf("Média de tempo de inserção: %.10f\n\n", (media/ 10e9));
-
-	    media = 0;
-
-	    // Buscar 10000 nodos
-	    for (int i = 0; i <= 10000; i++) {
-	      aux = System.nanoTime(); // Contagem de tempo de busca
-	      a.encontra(gerador.nextInt(MAX));
-	      media += System.nanoTime() - aux;
-	    }
-
-	    System.out.printf("Média de tempo de busca: %.10f\n\n", (media/ 10e9));
-
-	    chave = 1000; // A partir deste nodo, serão procurados 50
-	    aux = System.nanoTime(); // Contagem de tempo da busca de 50 nodos
-	    n50 = a.encontra50(chave);
-	    media += System.nanoTime() - aux;
-	    n50.grafico();
-
-	    System.out.printf("Média de tempo de busca de 50 nodos à partir de uma chave: %.10f\n\n", (media/ 10e9));
-
-
-	    total = System.nanoTime() - inicio;
-	    media /= 10000;
-
-
-	    System.out.printf("Tempo total: %.10f\n\n", (total/ 10e9));
-
-	     a.grafico();
-	  }
+	public static void main(String args[]) {
+		Arvore arv = new Arvore();
+		arv.adiciona(10);
+		arv.adiciona(20);
+		arv.adiciona(21);
+		//arv.adiciona(25);
+		arv.print();
+	}
 }
